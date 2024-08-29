187ff1e263dc5267d61c1db072ab697f334c2d78
dilvanrubens <74739920+dilvanrubens@users.noreply.github.com>
2024-08-29 17:04:16 -0300

    Add files via upload

---
+++ Hachatruck/Aulas/Aula1.0/Spotify/Spotify/SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyApp.swift
+//  Spotify
+//
+//  Created by Turma01-2 on 29/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
