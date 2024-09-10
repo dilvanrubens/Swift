b595e0d297df682b4bd3dc025d6ccd1ead8c0687
dilvanrubens <74739920+dilvanrubens@users.noreply.github.com>
2024-09-10 14:29:46 -0300

    Add files via upload

--- Hachatruck/PGTfinal/PGTfinal/ContentView.swift
+++ Hachatruck/PGTfinal/PGTfinal/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
                     ScrollView{
                         VStack(spacing: 7) {
                             
-                            ForEach(arrayNoticias) {s in
+                            ForEach(arrayNoticias) { s in
                                 NavigationLink(destination: Tela2(noticia: s)){
                                     VStack{
                                         AsyncImage(url: URL(string: s.capa)!) { image in
@@ -31,7 +31,6 @@ struct ContentView: View {
                                                 .scaledToFill()
                                                 .frame(width: 300, height: 150)
                                                 .clipped()
-                                                .foregroundStyle(.tint)
                                                 .padding(2)
                                                 .scaledToFill()
                                                 .clipped()
@@ -43,22 +42,25 @@ struct ContentView: View {
                                             HStack{
                                                 Image(systemName: "location.circle.fill")
                                                     .font(.system(size: 30))
+                                                    .tint(Color("verde"))
+                                                
                                                 Text(s.localizacao)
                                                     .foregroundStyle(.black)
                                                     .font(.system(size: 17))
-                                                    .multilineTextAlignment(.center)
                                                     .bold()
-                                                    .padding(5)
+                                                Spacer()
                                                 
-                                            }
+                                            }.multilineTextAlignment(.leading)
+                                                .padding(.bottom, 0.1)
                                             
-                                            Text(s.descricao + s.tempo)
-                                                .font(.system(size:15))
-                                                .frame(maxWidth: .infinity, alignment: .leading)
-                                                .foregroundStyle(.black)
+                                            HStack{
+                                                Text(s.descricao + s.tempo)
+                                                    .font(.system(size:15))
+                                                    .foregroundStyle(.black)
+                                            }.multilineTextAlignment(.leading)
                                             
                                             
-                                        }.padding(.init(top: 0, leading: 35, bottom: 0, trailing: 35))
+                                        }.padding(.init(top: 0, leading: 35, bottom: 10, trailing: 35))
                                     }
                                     
                                     
@@ -82,7 +84,7 @@ struct Noticia : Identifiable {
 }
 
 var arrayNoticias = [
-    Noticia(id: 1, localizacao: "Juazeiro do Norte, bairro Cidade Universitaria.", descricao: "Repentinamente um ataque zumbi destruiu todo o bairro", tempo: ", em 09/09/2024", capa: "https://images.adsttc.com/media/images/63ff/ceda/1a83/250f/afc0/e569/large_jpg/a-arquitetura-como-reflexo-da-condicao-humana-em-the-last-of-us_1.jpg?1677709037"),
+    Noticia(id: 1, localizacao: "Juazeiro do Norte e região do Cariri", descricao: "Queimadas continuam assolando Juazeiro do Norte e região do Cariri nos últimos dias", tempo: ", em 09/09/2024", capa: "https://images.adsttc.com/media/images/63ff/ceda/1a83/250f/afc0/e569/large_jpg/a-arquitetura-como-reflexo-da-condicao-humana-em-the-last-of-us_1.jpg?1677709037"),
     Noticia(id: 2, localizacao: "Local da notícia.", descricao: "Data da noticía junto à uma mini descrição", tempo: ", em 09/09/2009", capa: "https://images.adsttc.com/media/images/63ff/ceda/1a83/250f/afc0/e569/large_jpg/a-arquitetura-como-reflexo-da-condicao-humana-em-the-last-of-us_1.jpg?1677709037"),
     Noticia(id: 3, localizacao: "Local da notícia.", descricao: "Data da noticía junto à uma mini descrição", tempo: ", em 09/09/2009", capa: "https://images.adsttc.com/media/images/63ff/ceda/1a83/250f/afc0/e569/large_jpg/a-arquitetura-como-reflexo-da-condicao-humana-em-the-last-of-us_1.jpg?1677709037"),
     
--- Hachatruck/PGTfinal/PGTfinal/Tela2.swift
+++ Hachatruck/PGTfinal/PGTfinal/Tela2.swift
@@ -11,8 +11,8 @@ struct Tela2: View {@State var noticia: Noticia
     var body: some View {
    
         ZStack{
-            LinearGradient(colors: [.white, .green],
-                           startPoint: .top,endPoint: .bottomLeading).ignoresSafeArea()
+            LinearGradient(colors: [.verde, .white],
+                           startPoint: .top,endPoint: .center).ignoresSafeArea()
             ScrollView{
                 VStack {
                     
@@ -33,25 +33,26 @@ struct Tela2: View {@State var noticia: Noticia
                         
                         Text(noticia.localizacao)
                             .font(.title)
-                            .colorInvert()
+                            .foregroundColor(.black)
                             .bold()
                             .padding(5)
                         
                         Text(noticia.descricao+noticia.tempo)
-                            .colorInvert()
+                            .foregroundStyle(.black)
                             .bold()
+                        .padding(.bottom, 0.1)
                         
-                        Text("O desenvolvimento de The Last of Us começou em 2009 logo depois da finalização de Uncharted 2: Among Thieves, o título anterior da Naughty Dog. A relação entre Joel e Ellie foi o foco central do jogo, com todos os outros elementos de história e jogabilidade desenvolvendo-se ao seu redor. A equipe criativa escolheu os atores Troy Baker e Ashley Johnson a fim de interpretarem os personagens principais, que realizaram a captura de movimentos e dublagem para seus respectivos papéis. Ambos auxiliaram o diretor de criação Neil Druckmann com o desenvolvimento da história e personagens. Novos motores de jogo foram criados para satisfazer as necessidades técnicas, com a inteligência artificial de Ellie sendo um grande ponto de desenvolvimento e aprimoramento. A trilha sonora original foi composta por Gustavo Santaolalla, enquanto os efeitos sonoros assumiram um ponto de vista mais naturalista e minimalista do que em outros jogos de ação.")
-                            .colorInvert()
+                        Text("As queimadas continuam a assombrar Juazeiro do Norte e sua região circundante, causando preocupação e danos significativos. Nesta segunda-feira, 02, foi registrado relatos alarmantes das proximidades do Hotel Verdes Vale, onde as chamas consomem áreas florestais e lançam uma densa cortina de fumaça sobre a cidade. Mas Juazeiro do Norte não está sozinha nessa luta contra as chamas. O município de Caririaçu também enfrenta incêndios de grandes proporções. Sítios como São Lourenço, Cacimbas, Extrema e outros têm sido tomados por incêndios devastadores nos últimos cinco dias. Moradores locais relatam prejuízos severos, incluindo a perda de animais e colheitas, enquanto lutam para conter as chamas que ameaçam suas propriedades e meios de subsistência. O cenário é ainda mais sombrio em Juazeiro do Norte, onde a cidade está encoberta por uma densa camada de fumaça. As informações apontam que a fonte desse problema pode estar localizada nas proximidades de Caririaçu, onde os incêndios continuam a se alastrar descontroladamente. A situação se agrava ainda mais no distrito de Arajara, em Barbalha, onde os moradores do Sítio Taquari estão sofrendo com queimadas que já duram dois dias. Apesar da intervenção dos bombeiros, as chamas persistem, e o calor se intensifica cada vez mais na região.")
                             .bold()
-                            .colorInvert()
+                            .foregroundColor(.black)
+                            .font(.system(size: 15))
                             .multilineTextAlignment(.leading)
                         
                         //                        .frame(maxWidth: .infinity, alignment: .leading)
                     }.padding(.bottom, 150)
                     
-                }
-            }.padding()
+                }.padding(.init(top: 0, leading: 10, bottom: 0, trailing: 10))
+            }
         }
     }
 }
